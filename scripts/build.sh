2edb4c7ac970ca555c0085fddc664ccbf582fee6
Holly Liu <liuhong1.happy@163.com>
2026-04-09 16:01:04 +0800

    publish: 修改打包构建脚本

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 # 读取version文件
 VERSION=$(cat version)
 # 构建镜像
-docker build -t ghcr.io/gin-job/simple:latest ./examples/simple
+docker build -t ghcr.io/gin-job/simple:latest .
 # 标记镜像版本
 docker tag ghcr.io/gin-job/simple:latest ghcr.io/gin-job/simple:$VERSION
