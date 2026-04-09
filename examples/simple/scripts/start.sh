957c46f448db7afd392a53c13e8ca1cdc42a1c10
Holly Liu <liuhong1.happy@163.com>
2026-04-09 18:18:20 +0800

    publish: 更新脚本

--- examples/simple/scripts/start.sh
+++ examples/simple/scripts/start.sh
@@ -1,3 +1,4 @@
 service mysql start
-mysql -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 /app/gin-job
+
